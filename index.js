ad5f37300c10224340095e4973d07851aacb5340
FX <danielfx011@gmail.com>
2024-09-13 19:27:57 +0100

    fixing

---
+++ index.js
@@ -0,0 +1,22 @@
+const path = require("path");
+const config = require("./config");
+const { connect, modulesJS, patch } = require("./lib");
+const { fetchPlugins } = require("./lib/db/plugins");
+
+async function initialize() {
+	try {
+		await patch();
+		await modulesJS(path.join(__dirname, "/database/"));
+		console.log("Syncing Database");
+		await config.DATABASE.sync();
+		await modulesJS(path.join(__dirname, "/plugins/"));
+		await fetchPlugins();
+		console.log("Modules Installed");
+		return await connect();
+	} catch (error) {
+		console.error("Initialization error:", error);
+		return process.exit(1);
+	}
+}
+
+initialize();
