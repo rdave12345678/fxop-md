9cc9e4be87309d7c8595042075e8ded262bb3938
FX <danielfx011@gmail.com>
2024-09-14 17:53:08 +0100

    test

--- plugins/download.js
+++ plugins/download.js
@@ -1,4 +1,4 @@
-const { Module, mode } = require("../lib");
+const { Module, mode, toAudio } = require("../lib");
 const { ytPlay } = require("client");
 
 Module(
@@ -9,28 +9,34 @@ Module(
 		type: "download",
 	},
 	async (message, match, client) => {
-		if (!match) {
-			return message.sendReply(`\`\`\`Wrong Usage\n\n${message.prefix}video Just the two of us\n\nOR\n\n${message.prefix}video YOUTUBE URL\`\`\``);
-		}
+		if (!match) return message.sendReply(`\`\`\`Wrong Usage\n\n${message.prefix}video Just the two of us\`\`\``);
 
 		const smsg = await message.reply("*_Searching_*");
 		const {
 			details: { title, description },
 			video,
 		} = await ytPlay(match);
+		await message.send(video, { caption: `\`\`\`${description}\`\`\``, contextInfo: { forwardingScore: 999, isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: "120363327841612745@newsletter", newsletterName: title } } });
+		return smsg.edit(`*_Downloaded Successfully_*`);
+	},
+);
 
-		await message.send(video, {
-			caption: `\`\`\`${description}\`\`\``,
-			contextInfo: {
-				forwardingScore: 999,
-				isForwarded: true,
-				forwardedNewsletterMessageInfo: {
-					newsletterJid: "120363327841612745@newsletter",
-					newsletterName: title,
-				},
-			},
-		});
-
+Module(
+	{
+		pattern: "video",
+		fromMe: mode,
+		desc: "Fetches Music",
+		type: "download",
+	},
+	async (message, match, client) => {
+		if (!match) return message.sendReply(`\`\`\`Wrong Usage\n\n${message.prefix}play StarMan\`\`\``);
+		const smsg = await message.reply("*_Downloading_*");
+		const {
+			details: { title, description },
+			video,
+		} = await ytPlay(match);
+		const audio = await toAudio(video, "mp3");
+		await message.send(audio, { caption: `\`\`\`${description}\`\`\``, contextInfo: { forwardingScore: 999, isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: "120363327841612745@newsletter", newsletterName: title } } });
 		return smsg.edit(`*_Downloaded Successfully_*`);
 	},
 );
