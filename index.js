576d2d0a615804b7cc92f85072e76f6252a89000
FX <danielfx011@gmail.com>
2024-09-14 20:54:39 +0100

    fix

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
+const express = require("express");
 const path = require("path");
 const config = require("./config");
 const { connect, modulesJS, patch } = require("./lib");
 const { fetchPlugins } = require("./lib/db/plugins");
 
+const app = express();
+const PORT = 8000;
+
 async function initialize() {
 	try {
 		await patch();
 		await modulesJS(path.join(__dirname, "/lib/db/"));
-		console.log("Syncing Database");
 		await config.DATABASE.sync();
 		await modulesJS(path.join(__dirname, "/plugins/"));
 		await fetchPlugins();
 		console.log("Modules Installed");
-		return await connect();
+		await connect();
 	} catch (error) {
 		console.error("Initialization error:", error);
-		return process.exit(1);
+		process.exit(1);
 	}
 }
 
 initialize();
+
+app.listen(PORT);
