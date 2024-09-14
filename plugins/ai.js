7a48e0f2372d6a2edfddc1dc59f4abd901998e0c
FX <danielfx011@gmail.com>
2024-09-14 22:30:46 +0100

    Update ai.js

--- plugins/ai.js
+++ plugins/ai.js
@@ -10,7 +10,7 @@ ai.Module(
 		type: "ai gen",
 	},
 	async (message, match) => {
-		if (!match) return await message.sendReply("```Provide Me Words\n\n" + message.prefix + "genimg A cat and dog hugging each other.```");
+		if (!match) return await message.sendReply("```Provide Me Words\n\n" + message.prefix + "animegen A cat and dog hugging each other.```");
 		const image = await astro.animeGen(match);
 		await message.send(image, { caption: match, contextInfo: { forwardingScore: 999, isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: "120363327841612745@newsletter", newsletterName: "animegen" } } });
 	},
@@ -24,7 +24,7 @@ ai.Module(
 		type: "ai gen",
 	},
 	async (message, match) => {
-		if (!match) return await message.sendReply("```Provide Me Words\n\n" + message.prefix + "genimg A cat and dog hugging each other.```");
+		if (!match) return await message.sendReply("```Provide Me Words\n\n" + message.prefix + "dalle A cat and dog hugging each other.```");
 		const image = await astro.dalle(match);
 		await message.send(image, { caption: match, contextInfo: { forwardingScore: 999, isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: "120363327841612745@newsletter", newsletterName: "dalle" } } });
 	},
