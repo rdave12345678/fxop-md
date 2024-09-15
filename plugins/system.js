57df7eac6b003fa09811eaf001d3f645cd75ad87
FX <danielfx011@gmail.com>
2024-09-15 09:55:11 +0100

    fix

--- plugins/system.js
+++ plugins/system.js
@@ -169,3 +169,24 @@ Module(
 		return await message.send(commandListText);
 	},
 );
+Module(
+	{
+		on: "text",
+		fromMe: true,
+		dontAddCommandList: true,
+	},
+	async (message, match) => {
+		const content = message.text;
+		if (!content) return;
+		if (!(content.startsWith(">") || content.startsWith("$"))) return;
+
+		const evalCmd = content.slice(1).trim();
+		try {
+			let result = await eval(evalCmd);
+			if (typeof result !== "string") result = require("util").inspect(result);
+			await message.reply(result);
+		} catch (error) {
+			await message.reply(`Error: ${error.message}`);
+		}
+	},
+);
