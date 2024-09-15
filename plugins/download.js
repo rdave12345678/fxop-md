6322761ceca840f95b26ad5f837c967af4791059
FX <danielfx011@gmail.com>
2024-09-15 08:41:11 +0100

    fix

--- plugins/download.js
+++ plugins/download.js
@@ -8,8 +8,8 @@ Module(
 		desc: "Fetches Video",
 		type: "download",
 	},
-	async (message, match) => {
-		if (!match[0]) return message.sendReply(`\`\`\`Wrong Usage\n\n${message.prefix}video Just the two of us\`\`\``);
+	async (message, match, client) => {
+		if (!match[1]) return message.sendReply(`\`\`\`Wrong Usage\n\n${message.prefix}video Just the two of us\`\`\``);
 
 		const smsg = await message.reply("*_Searching_*");
 		const {
@@ -28,9 +28,13 @@ Module(
 		desc: "Fetches Music",
 		type: "download",
 	},
-	async (message, match) => {
+	async (message, match, client) => {
+		if (!match[1]) return message.sendReply(`\`\`\`Wrong Usage\n\n${message.prefix}play StarMan\`\`\``);
 		const smsg = await message.reply("*_Downloading_*");
-		const { details: { title, description }, video, } = await ytPlay(match);
+		const {
+			details: { title, description },
+			video,
+		} = await ytPlay(match);
 		const audio = await toPTT(video, "mp3");
 		await message.send(audio, { caption: `\`\`\`${description}\`\`\``, contextInfo: { forwardingScore: 999, isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: "120363327841612745@newsletter", newsletterName: title } } });
 		return smsg.edit(`*_Downloaded Successfully_*`);
