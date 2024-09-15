9f9f14253ca8632cfba0aa8bab7d1cdef23caf2d
FX <danielfx011@gmail.com>
2024-09-15 17:26:52 +0100

    fix

--- plugins/download.js
+++ plugins/download.js
@@ -27,14 +27,10 @@ Module(
 	},
 	async (message, match, client) => {
 		if (!match[1]) return message.sendReply(`\`\`\`Wrong Usage\n\n${message.prefix}video Just the two of us\`\`\``);
-
-		const smsg = await message.reply("*_Searching_*");
-		const {
-			details: { title, description },
-			video,
-		} = await ytPlay(match);
-		await message.send(video, { caption: `\`\`\`${description}\`\`\``, contextInfo: { forwardingScore: 999, isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: "120363327841612745@newsletter", newsletterName: title } } });
-		return smsg.edit(`*_Downloaded Successfully_*`);
+		const msg = await message.reply("*_Searching_*");
+		const { video } = await ytPlay(match);
+		await msg.edit(`*_Downloaded Success_*`);
+		return await message.send(video);
 	},
 );
 
@@ -47,13 +43,10 @@ Module(
 	},
 	async (message, match, client) => {
 		if (!match[1]) return message.sendReply(`\`\`\`Wrong Usage\n\n${message.prefix}play StarMan\`\`\``);
-		const smsg = await message.reply("*_Downloading_*");
-		const {
-			details: { title, description },
-			video,
-		} = await ytPlay(match);
+		const msg = await message.reply("*_Downloading_*");
+		const { video } = await ytPlay(match);
 		const audio = await toPTT(video, "mp3");
-		await message.send(audio, { caption: `\`\`\`${description}\`\`\``, contextInfo: { forwardingScore: 999, isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: "120363327841612745@newsletter", newsletterName: title } } });
-		return smsg.edit(`*_Downloaded Successfully_*`);
+		await msg.edit(`*_Downloaded Successfully_*`);
+		return await message.send(audio);
 	},
 );
