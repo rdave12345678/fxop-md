f11b6181d5e1e080856331ee4551c63bd2ac11ed
FX <danielfx011@gmail.com>
2024-09-15 08:34:46 +0100

    Update download.js

--- plugins/download.js
+++ plugins/download.js
@@ -29,12 +29,8 @@ Module(
 		type: "download",
 	},
 	async (message, match) => {
-		if (!message[1]) return message.sendReply(`\`\`\`Wrong Usage\n\n${message.prefix}play StarMan\`\`\``);
 		const smsg = await message.reply("*_Downloading_*");
-		const {
-			details: { title, description },
-			video,
-		} = await ytPlay(message[1]);
+		const { details: { title, description }, video, } = await ytPlay(match);
 		const audio = await toPTT(video, "mp3");
 		await message.send(audio, { caption: `\`\`\`${description}\`\`\``, contextInfo: { forwardingScore: 999, isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: "120363327841612745@newsletter", newsletterName: title } } });
 		return smsg.edit(`*_Downloaded Successfully_*`);
